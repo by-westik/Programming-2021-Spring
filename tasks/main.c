76f1318b66c103652c077fb76658ec45f849b294
by-westik <78916184+by-westik@users.noreply.github.com>
2021-02-15 23:26:32 +0300

    Попытка 2.0

--- tasks/main.c
+++ tasks/main.c
@@ -3,8 +3,7 @@
 #include "function.h"
 #include <string.h>
 
-const char *MSGS[] = {"0. Quit", "1. Removing even numbers in an array", "2. Finding the largest and smallest element in an array", "3. Line reversal", "4. Binary search"};
-const int MSGS_SIZE = sizeof(MSGS) / sizeof(MSGS[0]);
+const char *MSGS[] = {"0. Quit", "1. Removing even numbers in an array", "2. Finding the largest and smallest element i$const int MSGS_SIZE = sizeof(MSGS) / sizeof(MSGS[0]);
 
 int dialog(const char *msgs[], int n);
 void function_1();
@@ -12,7 +11,7 @@ void function_2();
 void function_3();
 void function_4();
 void enter_array(int **array, int *len);
-void array_print(int *array);
+void array_print(int *array, int len);
 char *enter_str();
 int array_check(int *array);
 
@@ -21,22 +20,23 @@ int main() {
     do {
         c = dialog(MSGS, MSGS_SIZE);
         switch(c) {
-            case 0:
-                break;
-            case 1:
-                function_1();
-                break;
-            case 2:
-                function_2();
-                break;
-            case 3:
-                function_3();
-                break;
-            case 4:
-                function_4();
-                break;
+        case 0:
+            break;
+        case 1:
+            function_1();
+            break;
+        case 2:
+            function_2();
+            break;
+        case 3:
+            function_3();
+            break;
+        case 4:
+            function_4();
+            break;
         }
     } while (c != 0);
+
     return 0;
 
 }
@@ -56,21 +56,20 @@ int dialog(const char *msgs[], int n) {
     } while (choice < 0 || choice >= n);
     return choice;
 }
+
 void function_1(){
     printf("***\nOption 1 - Removing even numbers in an array\n***\n");
-    int *array = NULL, *array_new = NULL;
+    int *array = NULL;
     int len;
-    printf("Enter array elements\n");
     enter_array(&array, &len);
     if((len - 1)!= 0){
-        array_print(array);
-        array_new = delete_even_numbers(array);
-        array_print(array_new);
+        array_print(array, len);
+        len = delete_even_numbers(array, len);
+        array_print(array, len);
     }else{
         printf("You entered an empty array. Try next time :(\n");
     }
     scanf("%*c");
-    free(array_new);
     free(array);
 }
 
@@ -78,33 +77,32 @@ void function_2(){
     printf("***\nOption 2 - Finding the largest and smallest element in an array\n***\n");
     int *array = NULL;
     int min, max, len;
-    printf("Enter array elements\n");
     enter_array(&array, &len);
     if((len - 1)!= 0){
-        array_print(array);
+        array_print(array, len);
         find(array, &min, &max);
-        printf("Minimum element in an array = %d\nMaximum element in an array = %d\n", min, max);
+        printf("min = %d\nmax = %d\n", min, max);
     }else{
         printf("You entered an empty array. Try next time :(\n");
     }
     scanf("%*c");
     free(array);
 }
+
 void function_3(){
     printf("***\nOption 3 - Line reversal\n***\n");
     char *str = NULL;
-    char *str_new = NULL;
     str = enter_str();
     if(strlen(str)) {
-        str_new = reverse_str(str);
-        printf("%s\n", str_new);
+        reverse_str(str);
+        printf("%s\n", str);
     } else {
         printf("You entered an empty line. Try next time :(\n");
     }
     free(str);
-    free(str_new);
-
 }
+
+
 
 void function_4(){
     printf("***\nOption 4 - Binary search in a sorted array\n***\n");
@@ -113,7 +111,7 @@ void function_4(){
     printf("Enter array elements in ascending order\n");
     enter_array(&array, &len);
     if((len - 1) != 0) {
-        array_print(array);
+        array_print(array, len);
         if (array_check(array) == 1) {
             printf("Enter the value of the element to find\n");
             scanf("%d", &n);
@@ -128,30 +126,25 @@ void function_4(){
     free(array);
 }
 
-
-void array_print(int *array){
-    int i = 0;
+void array_print(int *array, int len){
     printf("[ ");
-    while(array[i]){
+    for(int i = 0; i < len; i++){
         printf("%d ", array[i]);
-        i++;
     }
     printf("]\n");
 }
 
 void enter_array(int **array, int *len) {
-    int *new_array = (int*) calloc(3, sizeof(int));
     int n = 0;
-    int i = 0;
-    do{
-        printf("Write a number\n");
-        new_array = (int*) realloc(new_array, (i + 2)*sizeof(int));
-        scanf("%d", &n);
-        new_array[i] = n;
-        i++;
-    }while(n != 0);
+    printf("Enter the number of elements of the array\n");
+    scanf("%d", &n);
+    int *new_array = (int*) calloc(n + 1, sizeof(int));
+    for(int i = 0; i < n; i++){
+        printf("Enter the value of the array element\n");
+        scanf("%d", &new_array[i]);
+    }
     *array = new_array;
-    *len = i;
+    *len = n;
 }
 
 char *enter_str() {
