1920a71a94c9e8009b9a1489a43279ee888ece1f
by-westik <78916184+by-westik@users.noreply.github.com>
2021-02-15 23:27:08 +0300

    Попытка 2.0

--- tasks/function.h
+++ tasks/function.h
@@ -1,10 +1,9 @@
-
 #ifndef FUNCTION_DOT_H
 #define FUNCTION_DOT_H
 
-int *delete_even_numbers(int *array);
+int delete_even_numbers(int *array, int len);
 void find(int *array, int *min, int *max);
-char *reverse_str(char *str);
+void reverse_str(char *str);
 int binary_search(int *array, int n, int len);
 
 #endif
