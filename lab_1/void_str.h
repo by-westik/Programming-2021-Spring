77cd6ea4d107c3b3a472d83a913dd03ee59a28ab
by-westik <78916184+by-westik@users.noreply.github.com>
2021-03-09 13:07:39 +0300

    Изменили void_str.h
    Поменяла char *data на void *data

--- lab_1/void_str.h
+++ lab_1/void_str.h
@@ -1,17 +1,12 @@
 #include <stdlib.h>
 
 struct void_str{
-    size_t element_size; //Размер одного эелмента
-    size_t size; //Длина строки
-    size_t volume; //Это объем, выделенной под строчку памяти
-    char* data; //Тут будет значения элементов
-    //По идее тут нужен void* data, но когда я потом хочу обратиться, например, к элементу под 3 индексом,то увы( не получается
-    //потому что арифметические операции с void указетелем, как я поняла, не выполняются
-    //*Функция, возвращающая элемент по заданному индексу
+    size_t element_size;
+    size_t size;
+    size_t volume;
+    void* data;
     void* (*el_of_indx)(const struct void_str* const this, const size_t index);
-    //*Функция, помещающая новый элемент в конец строки
     void* (*push_back)(struct void_str* const this);
-    //*Функция, копирующая поршлую строку, возваращет потом указательна копию
     struct void_str* (*copy)(const struct void_str* const this);
 };
 
@@ -20,4 +15,3 @@ struct void_str* init_void_ptr_str(size_t size_of_str, size_t size_of_one_elemen
 
 void delete_void_str(struct void_str str);
 void delete_void_ptr_str(struct void_str* str);
-
