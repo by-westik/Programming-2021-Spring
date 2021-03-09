a5992bda368b2d05f18b4e7d6ec5984df4f6ca9c
by-westik <78916184+by-westik@users.noreply.github.com>
2021-03-09 13:08:38 +0300

    Изменили void_str.c

--- lab_1/void_str.c
+++ lab_1/void_str.c
@@ -5,7 +5,7 @@
 void* el_of_indx_void_str(const struct void_str* const this, const size_t index);
 void* push_back_void_str(struct void_str* const this);
 struct void_str* copy_void_str(const struct void_str* const this);
-char* data_alloc_void_str(size_t size_of_data, size_t size_of_element);
+void* data_alloc_void_str(size_t size_of_data, size_t size_of_element);
 void double_volume_void_str(struct void_str* const this);
 void copy_data_void_str(const struct void_str* const of, struct void_str* const in);
 void copy_element(void* of, void* in, const size_t size_of_element);
@@ -13,48 +13,48 @@ void delete_void_str(struct void_str str);
 void delete_void_ptr_str(struct void_str* str);
 
 int main(){
-    printf("Попытка\n\nСейчас попробуем сделать тесты\n");
+    printf("Попытка №3\n\nСейчас попробуем сделать тесты\n");
     struct void_str str = init_void_str(5, sizeof(void));
-//Я не знаю, как правильно тестить( НО попробуем
     printf("Исходная строка не void, а просто char - 'abcdf'\n");
     char proba[5] = "abcdf";
     for(int i = 0; i < 5; i++){
-        *(char*)str.el_of_indx(&str, i) = proba[i];//Как бы тут void, и, наверное, неправильно преобразовывать к char, но иначе выдает ошибку...
+        *(char*)str.el_of_indx(&str, i) = proba[i];
     }
     printf("Выведем получившуюся строчку\n");
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < str.size; i++){
         printf("%c", *(char*)str.el_of_indx(&str, i));
     }
+    *(char*)str.push_back(&str) = '\0';
+    printf("\nВыведем строчку по другому %s", (char*) str.data);
     printf("\nТеперь добавим еще 5 элементов к строке\n");
     char proba_2[5] = "ertyu";
     for(int i = 0; i < 5; i++){
         *(char*)str.push_back(&str) = proba_2[i];
     }
+    *(char*)str.push_back(&str) = '\0';
     printf("И снова выведем получившуюся строчку\n");
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < str.size; i++){
         printf("%c", *(char*)str.el_of_indx(&str, i));
     }
+    printf("\nВыведем строчку по другому %s", (char*) str.data);
     printf("\nПопробуем скопировать void строку в другую void строку\n");
     struct void_str* str_2 = str.copy(&str);
     printf("Выведем скопированную строчку\n");
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < str_2 -> size; i++){
         printf("%c", *(char*)str_2 -> el_of_indx(str_2, i));
     }
+    printf("\nВыведем строчку по другому %s", (char*) str_2 -> data);
     printf("\nТеперь добавим еще 3 элемента к новой строке\n");
     char proba_3[3] = "\tad";
     for(int i = 0; i < 3; i++){
         *(char*)str_2 -> push_back(str_2) = proba_3[i];
     }
+    *(char*)str_2 -> push_back(str_2) = '\0';
     printf("Измененная сокпированная строка\n");
-//А я могу у пользваотеля спрашивать, какой длины строку он хочет вести?
-//Или лучше сделать какой-то минимальный фиксированный размер, а потом его увеличивать, до тех пор пока не нажмется Enter?
-    for(int i = 0; i < 13; i++){
+    for(int i = 0; i < str_2 -> size; i++){
         printf("%c", *(char*)str_2 -> el_of_indx(str_2, i));
     }
-
-    for(int i = 0; i < 13; i++){
-        printf("%c", *(char*)str_2 -> el_of_indx(str_2, i));
-    }
+    printf("\nВыведем строчку по другому %s", (char*) str_2 -> data);
     printf("\nstr_2 -> element_size = %ld\nsizeof(str_2 -> element_size) = %ld\n", str_2 -> element_size, sizeof(str_2 -> element_size));
     printf("str_2 -> size = %ld\nsizeof(str_2 -> size) = %ld\n", str_2 -> size, sizeof(str_2 -> size));
     printf("str_2 -> volume = %ld\nsizeof(str_2 -> volume) = %ld\n", str_2 -> volume, sizeof(str_2 -> volume));
@@ -67,19 +67,17 @@ void* el_of_indx_void_str(const struct void_str* const this, const size_t index)
     if(index >= this -> size){
         return NULL;
     }
-    return &(this -> data[index * (this -> element_size)]);
+    void *data = (char*) this -> data;
+    for(size_t i = 0; i < index; i++){
+        *((char*)data++);
+    }
+    return data;
 }
 
 void copy_element(void* of, void* in, const size_t size_of_element){
-    char *one = (char*) of;//Вот тут как бы идет char, потому что логично, символы как бы на вряд ли будут другого типа
-    char *two = (char*) in;//Но по идее программа же должна работать со всеми типами, и какое-то нелогичное присваивание в void сразу к char.
-//Но я не знаю, как иначе сделать( + Вот цикл тоже вызывает вопросы, в том плане, что для копирования одного символа, он, конечно, не очень нужен
-//А вот если будут подаваться не символы, а что-нибудь другое, например, те же строки, то цикл может и нужен...
-    for(size_t i = 0; i < size_of_element; ++i){
-        two[i] = one[i];
+    for(size_t i = 0; i < size_of_element; i++){
+        *((char*) in + i) = *((char*) of + i);
     }
-//Но получается, когда я буду создавать char_str, то там преобразование к char, как выше, и надо делать?
-//И, соответсвенно, преобразовывать void к char функции тоже?
 }
 
 void copy_data_void_str(const struct void_str* const of, struct void_str* const in){
@@ -88,14 +86,14 @@ void copy_data_void_str(const struct void_str* const of, struct void_str* const 
     }
 }
 
-char* data_alloc_void_str(size_t size_of_data, size_t size_of_element){
+void* data_alloc_void_str(size_t size_of_data, size_t size_of_element){
     if(!size_of_data){
         return NULL;
     }
-    return (char *) malloc(size_of_data * size_of_element);
+    void *result = calloc(size_of_data + 1, size_of_element);
+    return result;
 }
 
-//Вот увеличение размера в два раза, чтобы, допустим, при добавлении элемента в конец не приходилось каждый раз увеличивать размер на 1, тут мне и объем и нужен
 void double_volume_void_str(struct void_str* const this){
     if(!(this -> volume)){
         this -> volume = 1;
@@ -112,7 +110,14 @@ void* push_back_void_str(struct void_str* const this){
     if(this -> size >= this -> volume){
         double_volume_void_str(this);
     }
-    ++(this -> size);
+    void *data = (char*) this -> data;
+    for(size_t i = 0; i < this -> size - 1; i++){
+        *((char*) data++);
+    }
+    if(*(char*) data == '\0'){
+        return this -> el_of_indx(this, this -> size - 1);
+    }
+    (this -> size)++;
     return this -> el_of_indx(this, this -> size - 1);
 }
 
