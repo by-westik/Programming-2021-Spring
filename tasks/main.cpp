db320014a978474767e2bb8c62dead4168962c5b
by-westik <78916184+by-westik@users.noreply.github.com>
2021-03-17 16:01:21 +0300

    Update main.cpp

--- tasks/main.cpp
+++ tasks/main.cpp
@@ -11,8 +11,8 @@ public:
     string name;
     int age;
     int weight;
-    Animal(string _name ="", int _age = 0, int _weight = 0)
-            :name(_name)
+    explicit Animal(string _name ="", int _age = 0, int _weight = 0)
+            :name(move(_name))
             ,age(_age)
             ,weight(_weight)
             {
@@ -24,38 +24,38 @@ public:
 class Cat: public Animal{
 public:
     string vote = "May-May";
-   Cat(string _name="", int _age = 0, int _weight = 0):Animal(_name, _age, _weight)
-   {
+    explicit Cat(string _name="", int _age = 0, int _weight = 0):Animal(move(_name), _age, _weight)
+    {
 
-   }
-    virtual string Say() override {return vote;}
+    }
+    string Say() override {return vote;}
 };
 
 class Dog: public Animal{
 public:
     string vote = "Gav-Gav";
-    Dog(string _name="", int _age = 0, int _weight = 0):Animal(_name, _age, _weight)
+    explicit Dog(string _name="", int _age = 0, int _weight = 0):Animal(move(_name), _age, _weight)
     {
 
     }
-    virtual string Say() override {return vote;}
+    string Say() override {return vote;}
 };
 
 class Kangaroo: public Animal{
 public:
     string vote = "Kyr-Kyr";
-    Kangaroo(string _name="", int _age = 0, int _weight = 0):Animal(_name, _age, _weight)
+    explicit Kangaroo(string _name="", int _age = 0, int _weight = 0):Animal(move(_name), _age, _weight)
     {
 
     }
-    virtual string Say() override {return vote;}
+    string Say() override {return vote;}
 };
 
 //Создаем рандомное имя
 string create_random_name(){
     int max =10, min =  3;
     int len = rand() % (max + 1 - min) + min;
-    string tmp_s;
+    string tmp_s;//То есть так строку лучше не создавать?
     static const char alphanum[] =
             "0123456789"
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -84,40 +84,42 @@ int create_random_weight(){
 }
 
 //Создаем вектор, состоящий только из Собак
-void create_dog_vector(vector <Dog*> *dog){
+/*
+void create_dog_vector(vector <Dog*> &dog){
     int max = 10, min = 5;
     int size = rand() % (max + 1 - min) + min;
     vector <Dog*> dogs(size);
     for(int i = 0; i < size; i++){
         dogs[i] = new Dog(create_random_name() + "_god", create_random_age(), create_random_weight());
     }
-    *dog = dogs;
-}
+    dog = dogs;
+}*/
 
 //Создаем вектор, состоящий только из Котов
-void create_cat_vector(vector <Cat*> *cat){
+void create_cat_vector(vector <Cat*> &cat){
     int max = 10, min = 5;
     int size = rand() % (max + 1 - min) + min;
     vector <Cat*> cats(size);
     for(int i = 0; i < size; i++){
         cats[i] = new Cat(create_random_name() + "_cat", create_random_age(), create_random_weight());
     }
-    *cat = cats;
+    cat = cats;
 }
 
 //Создаем вектор, состоящий только из Кенгуру
-void create_kangaroo_vector(vector <Kangaroo*> *kangaroo){
+/*
+void create_kangaroo_vector(vector <Kangaroo*> &kangaroo){
     int max = 10, min = 3;
     int size = rand() % (max + 1 - min) + min;
     vector <Kangaroo*> kangaroos(size);
     for(int i = 0; i < size; i++){
         kangaroos[i] = new Kangaroo(create_random_name() + "_kangaroo", create_random_age(), create_random_weight());;
     }
-    *kangaroo = kangaroos;
-}
+    kangaroo = kangaroos;
+}*/
 
 //Создаем вектор, который содержит все три вида животных
-void create_random_vector(vector <Animal*> *animal){
+void create_random_vector(vector <Animal*> &animal){
     int max = 23, min = 5;
     int size = rand() % (max + 1 - min) + min;
     vector <Animal*> animals(size);
@@ -130,12 +132,12 @@ void create_random_vector(vector <Animal*> *animal){
             animals[i] = new Kangaroo(create_random_name() + "_kangaroo", create_random_age(), create_random_weight());
         }
     }
-   *animal = animals;
+    animal = animals;
 }
 
 //Выводим элементы "смешанного" вектора на экран в формате - имя возраст вес
 void print_vector(vector <Animal*> animals){
-    for(int i = 0; i < animals.size(); i++){
+    for(int i = 0; i < animals.size(); ++i){
         cout << animals[i] -> name << " " << animals[i] -> age << " " << animals[i] -> weight << endl;
     }
     cout << endl;
@@ -143,7 +145,7 @@ void print_vector(vector <Animal*> animals){
 
 //Выводим элементы вектора котов в формате - имя возраст вес
 void print_cat(vector <Cat*> animals){
-    for(int i = 0; i < animals.size(); i++){
+    for(int i = 0; i < animals.size(); ++i){
         cout << animals[i] -> name << " " << animals[i] -> age << " " << animals[i] -> weight << endl;
     }
     cout << endl;
@@ -169,29 +171,28 @@ void say_animals(vector <Animal*> animal) {
 void max_animals(vector <Animal*> animal){
     int cat =0, dog = 0, kangaroo = 0;
     for(int i = 0; i < animal.size(); i++){
-        if(typeid(*animal[i]) == typeid(Cat)){
+        if(dynamic_cast <Cat*> (animal[i])){
             cat++;
-        } else if(typeid(*animal[i]) == typeid(Dog)){
+        } else if(dynamic_cast <Dog*> (animal[i])){
             dog++;
         } else {
             kangaroo++;
         }
     }
     cout << "Max type of animals = " << max(cat, max(dog, kangaroo)) << endl;
+    cout << endl;
 }
 
 //Находим 5 самых толстеньнких котиков
 void max_cats(vector <Animal*> animals){
-   sort(animals.begin(), animals.end(), [](const Animal *left, const Animal *right) {
-       return typeid(*left) == typeid(Cat);
-   });
-   sort(animals.rbegin(), animals.rend(), [](const Animal *left, const Animal *right){
-       return (typeid(*left) == typeid(Cat)) && (typeid(*right) == typeid(Cat)) && (left-> weight > right->weight);
-   });
-    //Не знаю, как это за один проход + иногда происходит ошибка, как ниже
-//Process finished with exit code -1073741819 (0xC0000005)
+    sort(animals.begin(), animals.end(), [](Animal *left, Animal *right) {
+        return dynamic_cast <Cat*> (left);
+    });
+    sort(animals.rbegin(), animals.rend(), [](Animal *left, Animal *right) {
+        return (dynamic_cast <Cat*> (left) && dynamic_cast <Cat*> (right)) && (left->weight > right->weight);
+    });
    int i = 0;
-   while(typeid(*animals[i]) == typeid(Cat)){
+   while(dynamic_cast <Cat*> (animals[i])){
        cout << animals[i] -> name << " " << animals[i] -> weight << endl;
        i++;
    }
@@ -201,10 +202,10 @@ void max_cats(vector <Animal*> animals){
 int main() {
     srand(time(NULL));
     vector <Animal*> animals;
-    create_random_vector(&animals);
+    create_random_vector(animals);
     print_vector(animals);
     vector <Cat*> cat;
-    create_cat_vector(&cat);
+    create_cat_vector(cat);
     print_cat(cat);
     int max_weight = the_biggest_animal(animals);
     cout << "The biggest weight is " << max_weight << endl;
