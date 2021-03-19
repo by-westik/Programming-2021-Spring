b74d0d8b8c4f26c91b1516151adc8644f1b8051f
by-westik <78916184+by-westik@users.noreply.github.com>
2021-03-19 23:52:30 +0300

    Update function_for_void_str.h

--- lab_1/function_for_void_str.h
+++ lab_1/function_for_void_str.h
@@ -5,3 +5,4 @@ struct void_str* concatenation(struct void_str* void_str_1, const struct void_st
 struct void_str* substring(const struct void_str* const this, const size_t start, const size_t end);
 size_t find_substr(const struct void_str* const this, const struct void_str* substring, int function(int));
 struct void_str* delete_all_sub_str(struct void_str* this, const struct void_str* const sub_str);
+struct void_str* random_void_str(size_t size, size_t size_of_element);
