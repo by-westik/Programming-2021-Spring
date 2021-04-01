3a6cff3a9bf4a47e2f655dc10594d21475448fbc
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-02 00:25:56 +0300

    Неудачная попытка разделить DynamicArray на файлы

--- lab_2/dynamicArray.cpp
+++ lab_2/dynamicArray.cpp
@@ -1,74 +1,75 @@
 #include <iostream>
 #include <cstdlib>
+#include "DynamicArray.h"
 
 using namespace std;
+//Я правильно объявляю конструктор? просто если написать DynamicArray(int _size = 0), то появляется ошибка
+//Создание массива, указанной длины
+template <class T> DynamicArray <T>:: DynamicArray(int _size)
+    :size(_size)
+    {
 
-template <class T> class DynamicArray{
-public:
-    T* array;
-    int size;
-    //Создание массива, указанной длины
-    DynamicArray(int _size = 0)
-            :size(_size)
-            ,array(new T(_size))
-            {
-
-            }
-    //Копирующий конструктор
-    DynamicArray(const T* _array, int _size)
-            :size(_size)
-            ,array(new T(_size))
-            {
-                for(int i = 0; i < _size; i++){
-                array[i] = _array[i];
-                }
-            }
-    //Копирующий конструктор, но он копирует не из простого массива, а из DynamicArray
-    DynamicArray(DynamicArray <T> const &dynamicArray)
-            :size(dynamicArray.size)
-            ,array(dynamicArray.array)
-            {
-
-            }
-    //Вывод массива на экран
-    void printArray(){
-        for (int i = 0; i < size; i++)
-            cout<< " " << array[i];
-        cout << endl;
-    };
-    //Получить элемент по индексу
-    T get(const int &index){
-        return array[index];
-    };
-    //Задать элемент по индексу
-    void set(int index, T value){
-        array[index] = value;
-    };
-    //Получить длину массива(Почему мы не можем просто делать, где надо DynamicArray.size&)
-    int getSize(){
-        return size;
     }
-    /* Изменить длину массива
-     * Если new_size > size - выделить память и скопировать туда предыдущую
-     * Если new_size < size - обрезать лишние элементы
-     */
-    void resize(const int &newSize){
-        if(size > newSize){
-            array = (T*) realloc(array, newSize * sizeof(T));
-        }else if (size < newSize){
-          //  T* newArray = array;
-            T* newArray  = (T*) realloc(array, newSize * sizeof(T));
-            array = newArray;
+
+//Создание динамического массива из статического массива
+template <class T> DynamicArray <T>::DynamicArray(const T *_array, int _size)
+    :size(_size)
+    ,array(new T(_size))
+    {
+        for(int i = 0; i < _size; i++){
+            array[i] = _array[i];
         }
-        size = newSize;
     }
-};
+
+
+//Копирующий конструктор
+template <class T> DynamicArray <T>::DynamicArray(const DynamicArray<T> &dynamicArray)
+    :size(dynamicArray.size)
+    ,array(dynamicArray.array)
+    {
+
+    }
+
+//Вывод массива на экран
+template <class T> void DynamicArray <T>::printArray() {
+    for (int i = 0; i < size; i++)
+        cout<< " " << array[i];
+    cout << endl;
+}
+
+//Получить элемент по индексу
+template <class T> const T& DynamicArray <T>::get(int index) {
+    return array[index];
+}
+
+//Задать элемент по индексу
+template <class T> void DynamicArray <T>::set(int index, T value) {
+    array[index] = value;//Я все сломала( На этом моменте появляется ошибка сегментации
+    // (ಥ﹏ಥ) памагите, пожалуйста
+}
+
+//Получить длину массива
+template <class T> int DynamicArray <T>::getSize() {
+    return size;
+}
+
+//Изменить размер массива
+template <class T> void DynamicArray <T>::resize(int newSize) {
+    if(size > newSize){
+        array = (T*) realloc(array, newSize * sizeof(T));
+    }else if (size < newSize){
+        T* newArray  = (T*) realloc(array, newSize * sizeof(T));
+        array = newArray;
+    }
+    size = newSize;
+}
 
 int main() {
     cout << "Int array" << endl;
     int arr[5] = {1, 2, 3, 4, 5};
     DynamicArray<int> a(arr, 5);
     a.printArray();
+    //cout << a.get(3) << endl;
     DynamicArray<int> b(7);
     for(int i = 0; i < 7; i++){
         b.set(i, i);
