1217501007c873da664398109b8befd6414d8d81
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-07 00:00:42 +0300

    *вжух* и заработало (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧

--- lab_2/Sequence.h
+++ lab_2/Sequence.h
@@ -11,7 +11,7 @@ public:
     virtual T& getFirst() = 0;
     virtual T& getLast() = 0;
     virtual T& get(int index) = 0;
-   // virtual Sequence <T>* getSubsequence(const int startIndex,const int endIndex) = 0;
+   // virtual Sequence <T>* getSubsequence(int startIndex,int endIndex) = 0;
     virtual int getLength() = 0;
     virtual void append(T item) = 0;
     virtual void prepend(T item) = 0;
@@ -21,125 +21,132 @@ public:
 
 template <class T> class LinkedListSequence : public Sequence <T>{
 public:
-    LinkedList <T> data;
+    LinkedList <T> *data;
     LinkedListSequence(const T* _list, int _size)
-    :data(LinkedList<T>(_list, _size))
+    :data(new LinkedList<T>(_list, _size))
     {
 
     };
     LinkedListSequence()
-    :data(LinkedList<T>(NULL, 0))
+    :data(new LinkedList<T>())
     {
 
     };
     LinkedListSequence(LinkedListSequence <T> &listSequence)
-    :data(LinkedList<T>(listSequence.data))
+    :data(new LinkedList<T>(*listSequence.data))
     {
 
     };
     T& getFirst() override
     {
-        return this -> data.getFirst();
+        return this -> data -> getFirst();
     };
     T& getLast() override
     {
-        return this -> data.getLast();
+        return this -> data -> getLast();
     };
     T& get(int index) override
     {
-        return this -> data.get(index);
+        return this -> data -> get(index);
     };
     int getLength() override
     {
-        return this -> data.getLength();
+        return this -> data -> getLength();
     };
     void append(T item) override
     {
-        data.append(item);
+        data -> append(item);
     };
     void prepend(T item) override
     {
-        data.prepend(item);
+        data -> prepend(item);
     };
     void insertAt(T item, int index) override{
-        data.insertAt(item, index);
-    };//Пока косячно вставляется, но я исправлю
-    void printLinkedListSequence(){
-        this -> data.printList();
+        data -> insertAt(item, index);
     };
+    void printLinkedListSequence(){
+        this -> data -> printList();
+    };
+
+    Sequence <T>* getSubsequence(int startIndex,int endIndex){
+        LinkedListSequence<T> *subSequence = new LinkedListSequence<T>();
+        subSequence -> data = this -> data -> getSubList(startIndex, endIndex);
+        return dynamic_cast <Sequence<T>*> (subSequence);
+    }
 };
 
 template <class T> class ArraySequence :public Sequence<T>{
 
 public:
-    DynamicArray <T> data;
+    DynamicArray <T> *data;
     ArraySequence(const T* _array, int _size)
-    :data((DynamicArray<T>(_array, _size)))
+    :data(new DynamicArray<T>(_array, _size))
     {
 
     };
     ArraySequence()
-    :data(DynamicArray<T>(0))
+    :data(new DynamicArray<T>(0))
     {
 
     };
     ArraySequence(ArraySequence<T> &arraySequence)
-    :data(DynamicArray<T>(arraySequence.data))
+    :data(new DynamicArray<T>(*arraySequence.data))
     {
 
     };
     T& getFirst() override
     {
-        return this -> data.get(0);
+        return this -> data -> get(0);
     };
     T& getLast() override
     {
-        return this -> data.get(data.size - 1);
+        return this -> data -> get(data -> size - 1);
     };
     T& get(int index) override
     {
-        return this -> data.get(index);
+        return this -> data -> get(index);
     };
- /*   Sequence <T>* getSubsequence(const int startIndex,const int endIndex) override
+    Sequence <T>* getSubsequence(int startIndex,int endIndex)
     {
-        ArraySequence <T> *subSequence;// (this -> data.array, endIndex - startIndex);
+        ArraySequence <T> *subSequence = new ArraySequence<T>();
         int j = 0;
+        subSequence -> data ->resize(endIndex - startIndex);
         for(int i = startIndex; i < endIndex; i++){
-            subSequence -> data.array[j] = std::move(data.array[i]);
+            subSequence -> data -> set(j, this -> data -> array[i]);
             j++;
         }
         return dynamic_cast <Sequence<T>*> (subSequence);
-    };//Эта функция не работает, но я еще исправлю*/
+    };
 
     int getLength() override
     {
-        return this -> data.getSize();
+        return this -> data -> getSize();
     };
     void append(T item) override
     {
-        data.resize(data.size + 1);
-        for(int i = data.size - 1; i > 0; i--){
-            data.set(i, data.get(i - 1));
+        data -> resize(data -> size + 1);
+        for(int i = data -> size - 1; i > 0; i--){
+            data -> set(i, data -> get(i - 1));
         }
-        data.set(0, item);
+        data -> set(0, item);
     };
     void prepend(T item) override
     {
-        data.resize(data.size + 1);
-        data.set(data.size - 1, item);
+        data -> resize(data -> size + 1);
+        data -> set(data -> size - 1, item);
     };
     void insertAt(T item, int index) override{
-        data.resize(data.size + 1);
-        for(int i = data.size - 1; i > index; i--){
-            data.set(i, data.get(i - 1));
+        data -> resize(data -> size + 1);
+        for(int i = data -> size - 1; i > index; i--){
+            data -> set(i, data ->get(i - 1));
         }
-        data.set(index, item);
+        data -> set(index, item);
     };
     /*Sequence <T>* concat(Sequence <T> *list) override{
 
     };*/
     void printArraySequence(){
-        this -> data.printArray();
+        this -> data -> printArray();
     };
 };
 
