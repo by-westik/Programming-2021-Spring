440d82e86bb7b0e9b470f17439e59b1c75f95bb5
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-19 23:43:36 +0300

    Добавление простого меню

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
+#include <string>
+#include <time.h>
 #include "Sequence.h"
 #include "Stack.h"
+#include "FunctionForMap.h"
+#include "menu.cpp"
 using namespace std;
+/*
+class Student{
+private:
+    string *firstName;
+    string *middleName;
+    string *lastName;
+    time_t birthDate;
+    string *group;
+public:
+    string *getFirstName(){
+        return firstName;
+    };
+    Student(string *_firstName = NULL)//"", string *_middleName = "", string *lastName = "", time_t _birthDate, string *_group = "")
+    :firstName(std::move(_firstName)){};
+};*/
 
 int main () {
-    Stack<int> st;
-    st.push(10);
-    st.printStack();
-    st.push(30);
-    st.printStack();
-    st.push(20);
-    st.printStack();
-    st.push(5);
-    st.printStack();
-    st.push(1);
-    st.printStack();
-    st.map();
-    cout << endl;
-    st.printStack();
-    Stack<int> sub_st(*st.subStack(2,4));
-    sub_st.printStack();
-    Stack<int> con_st(*st.concatStack(sub_st));
-    con_st.printStack();
+    menu();
     return 0;
 }
