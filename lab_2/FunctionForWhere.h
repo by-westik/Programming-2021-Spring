300bb481e9126be65261d0e73fa848c5232a1ecd
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-22 00:01:16 +0300

    Функции для where(int, float)

---
+++ lab_2/FunctionForWhere.h
@@ -0,0 +1,21 @@
+
+
+#ifndef LABA_2_FUNCTIONFORWHERE_H
+#define LABA_2_FUNCTIONFORWHERE_H
+
+template <typename T> bool isBigger(T a, T b){
+    return a > b;
+}
+
+template <typename T> bool isPositive(T a){
+    return a > 0;
+}
+
+template <typename T> bool isNegative(T a){
+    return a < 0;
+}
+
+#endif //LABA_2_FUNCTIONFORWHERE_H
+
+
+
