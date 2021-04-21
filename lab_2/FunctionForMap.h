cce007cda0fa3bd95be032613e7512cdd638460f
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-21 23:59:45 +0300

    Функции для map(int, float)

---
+++ lab_2/FunctionForMap.h
@@ -0,0 +1,26 @@
+//
+// Created by User on 10.04.2021.
+//
+
+#ifndef LABA_2_FUNCTIONFORMAP_H
+#define LABA_2_FUNCTIONFORMAP_H
+
+template <typename T> T multiply(T a, T b){
+    return a * b;
+}
+
+template <typename T> T division(T a, T b){
+    return a / b;
+}
+
+template <typename T> T addition(T a, T b){
+    return a + b;
+}
+
+template <typename T> T subtraction(T a, T b){
+    return a - b;
+}
+
+#endif //LABA_2_FUNCTIONFORMAP_H
+
+
