d63426db8bb3bc95ff3744016fa2c71d3d9b070c
by-westik <78916184+by-westik@users.noreply.github.com>
2021-04-24 00:36:29 +0300

    Помогите пожалуйста (｡•́︿•̀｡)

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -1,28 +1,37 @@
-#include <iostream>
-#include <string>
-#include <time.h>
-#include "Sequence.h"
-#include "Stack.h"
-#include "FunctionForMap.h"
-#include "menu.cpp"
-using namespace std;
+
+template <class T, template <class> class Sequence> void createStack(Stack <T, Sequence> &stack){
+    std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
+    int len = 0;
+    while(len <= 0){
+        std::cin >> len;
+    }
+    for(int i = 0; i < len; i++){
+        std::cout << "Введите элемент" << std::endl;
+        T n = 0;
+        std::cin >> n;
+        stack.push(n);
+    }
+};
 /*
-class Student{
-private:
-    string *firstName;
-    string *middleName;
-    string *lastName;
-    time_t birthDate;
-    string *group;
-public:
-    string *getFirstName(){
-        return firstName;
-    };
-    Student(string *_firstName = NULL)//"", string *_middleName = "", string *lastName = "", time_t _birthDate, string *_group = "")
-    :firstName(std::move(_firstName)){};
-};*/
+template <> void createStack <Complex, LinkedListSequence> (Stack <Complex, LinkedListSequence> &stack){
+    std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
+    int len = 0;
+    while(len <= 0){
+        std::cin >> len;
+    }
+    for(int i = 0; i < len; i++){
+        std::cout << "Введите x" << std::endl;
+        double r, im;
+        std::cin >> r;
+        std::cout << "Введите y" << std::endl;
+        std::cin >> im;
+        stack.push(Complex(r,im));
+    }
+}
 
 int main () {
-    menu();
-    return 0;
-}
+    Stack <int, LinkedListSequence> n;
+  //  n.createStack();
+    createStack <int, LinkedListSequence> (n);
+    n.printStack();
+ }
