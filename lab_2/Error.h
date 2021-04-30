371cc85f46cc86d2ba8d24b9433487be0282bc4b
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-01 00:02:31 +0300

    Добавление класса для обработки ошибок

---
+++ lab_2/Error.h
@@ -0,0 +1,13 @@
+#ifndef LABA_2_ERROR_H
+#define LABA_2_ERROR_H
+
+class Error{
+private:
+    std::string error;
+public:
+    Error(std::string _error);
+    std::string getError();
+};
+
+#endif //LABA_2_ERROR_H
+
