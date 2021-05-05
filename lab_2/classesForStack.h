6255fe72afd153f3873f01523a38e815baf124a0
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-05 23:06:23 +0300

    Update classesForStack.h

--- lab_2/classesForStack.h
+++ lab_2/classesForStack.h
@@ -15,8 +15,15 @@ public:
     std::string getMiddleName() const;
     std::string getLastName() const;
     int getAge() const;
+    void setFirstName(std::string _firstName);
+    void setMiddleName(std::string _middleName);
+    void setLastName(std::string _lastName);
+    void setAge(int _age);
+    Person(){};
     Person(int _age, std::string _firstName, std::string _middleName, std::string _lastName);
     void printPerson() const;
+    void createPerson();
+    bool operator== (Person &person) const;
 };
 
 class Student: public Person{
@@ -24,21 +31,26 @@ private:
     std::string group;
 public:
     std::string getGroup() const;
+    void setGroup(std::string _group);
+    Student(){};
     Student(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _group);
     void printStudent() const;
     bool operator== (Student &_student) const;
     bool operator> (Student &_student) const;
 };
+
 class Teacher: public Person{
 private:
     std::string subject;
 public:
     std::string getSubject() const;
+    Teacher(){};
     Teacher(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _subject);
     void printTeacher() const;
     bool operator== (Teacher &_teacher) const;
     bool operator> (Teacher &_teacher) const;
 };
+
 
 class Complex{
 private:
@@ -51,6 +63,7 @@ public:
     Complex operator- (const Complex &_complex) const;
     Complex operator* (const Complex &_complex) const;
     Complex operator/ (const Complex &_complex) const;
+    Complex& operator+= (const Complex &_complex);
     bool operator== (const Complex &_complex) const;
     bool operator< (const Complex &_complex) const;
     bool operator> (const Complex &_complex) const;
