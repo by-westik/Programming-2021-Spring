41e9aed269f019aa56e26369fd55c112ad2955d9
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-05 23:05:26 +0300

    Переопределение >> << ....

--- lab_2/classesForStack.cpp
+++ lab_2/classesForStack.cpp
@@ -32,6 +32,69 @@ int Person::getAge() const{
     return age;
 };
 
+void Person::setFirstName(std::string _firstName){
+    this -> firstName = _firstName;
+};
+
+void Person::setMiddleName(std::string _middleName){
+    this -> middleName = _middleName;
+};
+
+void Person::setLastName(std::string _lastName){
+    this -> lastName = _lastName;
+};
+
+void Person::setAge(int _age){
+    this -> age = _age;
+};
+
+bool Person::operator== (Person &_person) const{
+    return ((this -> getFirstName() == _person.getFirstName()) && (this -> getLastName() == _person.getLastName())
+        && (this -> getMiddleName() == _person.getMiddleName()) && (this -> getAge() == _person.getAge()));
+};
+// Эти перегрузки оператора работают, но есть немного ниже вопрос
+/*
+std::istream& operator>> (std::istream &in, Person &_person){
+    std::cout << "Введите имя"<< std::endl;
+    std::string _firstName, _middleName, _lastName;
+    in >> _firstName;
+    _person.setFirstName(_firstName);
+    std::cout << "Введите фамилию"<< std::endl;
+    in >> _lastName;
+    _person.setLastName(_lastName);
+    std::cout << "Введите отчество"<< std::endl;
+    in >> _middleName;
+    _person.setMiddleName(_middleName);
+    int _age;
+    std::cout << "Введите возраст"<< std::endl;
+    in >> _age;
+    _person.setAge(_age);
+    return in;
+};*/
+
+void Person::createPerson(){
+    std::cout << "Введите имя"<< std::endl;
+    std::string _firstName, _middleName, _lastName;
+    std::cin >> _firstName;
+    this -> setFirstName(_firstName);
+    std::cout << "Введите фамилию"<< std::endl;
+    std::cin >> _lastName;
+    this -> setLastName(_lastName);
+    std::cout << "Введите отчество"<< std::endl;
+    std::cin >> _middleName;
+    this -> setMiddleName(_middleName);
+    int _age;
+    std::cout << "Введите возраст"<< std::endl;
+    std::cin >> _age;
+    this -> setAge(_age);
+};
+
+std::istream& operator>> (std::istream &in, Person &_person){
+    _person.createPerson();
+    return in;
+};
+
+
 Student::Student(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _group)
     :Person(_age, _firstName, _middleName, _lastName)
     ,group(std::move(_group))
@@ -42,6 +105,10 @@ std::string Student::getGroup() const{
     return group;
 };
 
+void Student::setGroup(std::string _group){
+    this -> group = _group;
+};
+
 void Student::printStudent() const{
     printPerson();
     std::cout << "Group - " << getGroup() << std::endl;
@@ -50,11 +117,24 @@ void Student::printStudent() const{
 bool Student::operator==(Student &_student) const{
     return ((this -> getFirstName() == _student.getFirstName()) && (this -> getLastName() == _student.getLastName())
         && (this -> getMiddleName() == _student.getMiddleName()) && (this -> getAge() == _student.getAge()) && (this -> getGroup() == _student.getGroup()));
+//    return ((*this == _student) && (this -> getGroup() == _student.getGroup()));
+// Я никак не смогу использовать == перегруженный в Person для == в Student/Teacher?
 };
 
 bool Student::operator>(Student &_student) const{
     return (this -> getAge() > _student.getAge());
 };
+
+std::istream& operator>> (std::istream &in, Student &_student){
+ //   in >> _student; //И тут тоже не получиться вызвать >> для Person?
+    _student.createPerson();
+    std::string _group;
+    std::cout << "Введите группу" << std::endl;
+    in >> _group;
+    _student.setGroup(_group);
+    return in;
+};
+
 Teacher::Teacher(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _subject)
     :Person(_age, _firstName, _middleName, _lastName)
     ,subject(std::move(_subject))
@@ -74,6 +154,7 @@ bool Teacher::operator==(Teacher &_teacher) const{
     return ((this -> getFirstName() == _teacher.getFirstName()) && (this -> getLastName() == _teacher.getLastName())
         && (this -> getMiddleName() == _teacher.getMiddleName()) && (this -> getAge() == _teacher.getAge()) && (this -> getSubject() == _teacher.getSubject()));
 };
+
 bool Teacher::operator>(Teacher &_teacher) const{
     return (this -> getAge() > _teacher.getAge());
 };
@@ -90,6 +171,7 @@ Complex Complex::operator+ (const Complex &_complex) const{
     result.imaginary = imaginary + _complex.imaginary;
     return result;
 };
+
 Complex Complex::operator- (const Complex &_complex) const{
     Complex result;
     result.real = real - _complex.real;
@@ -111,6 +193,12 @@ Complex Complex::operator/ (const Complex &_complex) const{
     return result;
 };
 
+Complex& Complex::operator+= (const Complex &_complex){
+    real += _complex.real;
+    imaginary += _complex.imaginary;
+    return *this;
+};
+
 bool Complex::operator== (const Complex &_complex) const{
     return ((real == _complex.real) && (imaginary == _complex.imaginary));
 };
@@ -121,6 +209,23 @@ bool Complex::operator< (const Complex &_complex) const{
 
 bool Complex::operator> (const Complex &_complex) const{
     return ((real > _complex.real) && (imaginary > _complex.imaginary));
+};
+
+std::istream& operator>> (std::istream &in, Complex &complex){
+    std::cout << "Введите реальную часть числа"<< std::endl;
+    double _real;
+    in >> _real;
+    complex.setReal(_real);
+    std::cout << "Введите мнимую часть числа"<< std::endl;
+    double _imaginary;
+    in >> _imaginary;
+    complex.setImaginary(_imaginary);
+    return in;
+};
+
+std::ostream& operator<< (std::ostream &out, const Complex &complex){
+    out << complex.getReal() << "x + " << complex.getImaginary() << "y" << std::endl;
+    return out;
 };
 
 double Complex::getReal() const{
@@ -144,4 +249,5 @@ void Complex::printComplex() const{
 };
 
 #endif //LABA_2_CLASSESFORSTACK_H
+
 
