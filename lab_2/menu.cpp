3100a78835b1e596732b82ca55ce587f138beb66
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-05 23:19:35 +0300

    Теперь функция ввода работает для всех типов)

--- lab_2/menu.cpp
+++ lab_2/menu.cpp
@@ -1,71 +1,31 @@
+//
+// Created by User on 10.04.2021.
+//
+
 #include "Stack.h"
 #include "Sequence.h"
-const char *MENU[] = {"0.Exit", "1.Int", "2.Float", "3.Complex numbers" "4.Char", "5.Function" "6.Students", "7.Teachers"};
+
+const char *MENU[] = {"0.Exit", "1.Int", "2.Double", "3.Complex numbers", "4.Char", "5.Function", "6.Students", "7.Teachers"};
 const int MENU_SIZE = sizeof(MENU) / sizeof(MENU[0]);
 
-const char *FUNCTION[] = {"0.Exit", "1. Map", "2.Where", "3.Reduce", "4.Conatenation", "5.SubSequence(index)", "6.Find SubSequence"};
+const char *FUNCTION[] = {"0.Exit", "1.Map", "2.Where", "3.Reduce", "4.Conatenation", "5.SubSequence(index)", "6.Find SubSequence"};
 const int FUNCTION_SIZE = sizeof(FUNCTION) / sizeof(FUNCTION[0]);
 
-const char *MAP_FOR_INT_FLOAT_COMPLEX[] = {"1.Addition", "2.Subtraction", "3.Multiplication", "4.Division"};
-const int MAP_FOR_INT_FLOAT_COMPLEX_SIZE = sizeof(MAP_FOR_INT_FLOAT_COMPLEX) / sizeof(MAP_FOR_INT_FLOAT_COMPLEX[0]);
+const char *MAP[] = {"0.Exit", "1.Addition", "2.Subtraction", "3.Multiplication", "4.Division"};
+const int MAP_SIZE = sizeof(MAP) / sizeof(MAP[0]);
 
-int dialog(const char *msgs[], const int size){
-    for(int i = 0; i < size; i++){
-        std::cout << msgs[i] << std::endl;
-    }
-    std::cout << "Make your choice:" << std::endl;
-    int choice;
-    std::cin >> choice;
-    return choice;
-};
-//Я еще не знаю, как лучше сделать  меню, но, наверное, буду передавать указатели на функции
-template <class T> T functionForIntFloat(Stack <T, Sequence> *stack){
-    std::cout << "What!?" << std::endl;
-    bool choiceFunction = true;
-    int a = 0;
-    while(choiceFunction){
-        a = dialog(FUNCTION, FUNCTION_SIZE);
-        switch (a) {
-            case 1:
-                std::cout << "Map" << std::endl;
-                break;
-            case 2:
-                 std::cout << "Where" << std::endl;
-                 break;
-            case 3:
-                std::cout << "Reduce" << std::endl;
-                break;
-            case 4:
-                std::cout << "Concatenation" << std::endl;
-                break;
-            case 5:
-                std::cout << "SubSequence(index)" << std::endl;
-                break;
-            case 6:
-                std::cout << "Find SubSequence" << std::endl;
-                break;
-            case 0:
-                std::cout << "Exit" << std::endl;
-                choiceFunction = false;
-                break;
-            default:
-                std::cout << "Error" << std::endl;
-                choiceFunction = false;
-                break;
-        }
-    }
-};
-/*
-typename <class T> choiceFunction(Stack <class T, Sequence> *stack){
-    bool choiceFunction = true;
-    int a = 0;
-    while(choiceFunction){
-        a = dialog(FUNCTION, FUNCTION_SIZE);
-        switch(a){
+const char *WHERE[] = {"0.Exit", "1.IsSmaller", "2.IsBigger", "3.IsEqual", "4.IsUnEqual"};
+const int WHERE_SIZE = sizeof(WHERE) / sizeof(WHERE[0]);
 
-        }
-    }
-}*/
+int dialog(const char *msgs[], const int size);
+template <class T, template <class> class Sequence> void createStack(Stack <T, Sequence> &stack);
+template <class T, template <class> class Sequence> void functionChoisenWhere(Stack <T, Sequence> *stack,  bool function(T, T));
+template <class T, template <class> class Sequence> void functionForWhere(Stack <T, Sequence> *stack);
+template <class T, template <class> class Sequence> void functionChoisenMap(Stack <T, Sequence> *stack,  T function(T, T));
+template <class T, template <class> class Sequence> void functionChoisenReduce(Stack <T, Sequence> *stack,  T function(T, T));
+template <class T, template <class> class Sequence> void functionForMapReduce(Stack <T, Sequence> *stack, void functionChoisen(Stack <T, Sequence> *stack, T function (T, T)));
+template <class T, template <class> class Sequence> void functionForStack(Stack <T, Sequence> *stack);
+
 void menu(){
     bool choiceType = true;
     int a = 0;
@@ -76,32 +36,62 @@ void menu(){
                 {
                     std::cout << "Int" << std::endl;
                     Stack <int, LinkedListSequence> *stackInt = new Stack <int, LinkedListSequence>();
-                    functionForIntFloat(dynamic_cast <Stack <int, Sequence>*> (stackInt));
+                    createStack <int, LinkedListSequence> (*stackInt);
+                    stackInt -> printStack();
+                    functionForStack <int, LinkedListSequence> (stackInt);
                     delete stackInt;
                 }
                 break;
             case 2:
                 {
-                    std::cout << "Float" << std::endl;
-                    Stack <float, LinkedListSequence> *stackInt = new Stack <float, LinkedListSequence>();
-                    functionForIntFloat(dynamic_cast <Stack <float, Sequence>*> (stackInt));
-                    delete stackInt;
+                    std::cout << "Double" << std::endl;
+                    Stack <double, LinkedListSequence> *stackFloat = new Stack <double, LinkedListSequence>();
+                    createStack <double, LinkedListSequence> (*stackFloat);
+                    stackFloat -> printStack();
+                    functionForStack <double, LinkedListSequence>(stackFloat);
+                    delete stackFloat;
                 }
                 break;
             case 3:
-                std::cout << "Complex numbers" << std::endl;
+                {
+                    std::cout << "Complex numbers" << std::endl;
+                    Stack <Complex, LinkedListSequence> *stackComplex = new Stack <Complex, LinkedListSequence>();
+                    createStack <Complex, LinkedListSequence> (*stackComplex);
+                    stackComplex -> printStack();
+                    functionForStack <Complex, LinkedListSequence> (stackComplex);
+                    delete stackComplex;
+                }
                 break;
             case 4:
-                std::cout << "Char" << std::endl;
+                {
+                    std::cout << "Char" << std::endl;
+                    Stack <char, LinkedListSequence> *stackChar = new Stack <char, LinkedListSequence>();
+                    createStack <char, LinkedListSequence> (*stackChar);
+                    stackChar -> printStack();
+                    functionForStack <char, LinkedListSequence>(stackChar);
+                    delete stackChar;
+                }
                 break;
             case 5:
                 std::cout << "Function" << std::endl;
+                std::cout << "Coming soon" << std::endl;
                 break;
             case 6:
-                std::cout << "Students" << std::endl;
+                {
+                    std::cout << "Students" << std::endl;
+                    Stack <Student, LinkedListSequence> *stackStudent = new Stack <Student, LinkedListSequence>();
+                    createStack <Student, LinkedListSequence> (*stackStudent);
+                    stackStudent -> printStack();
+                  //  Student n;
+                 //   std::cin >> n;
+              //      functionChoisenWhere(stackStudent, isEqual);
+//                    functionForStack <Student, LinkedListSequence>(stackStudent);
+                    delete stackStudent;
+                }
                 break;
             case 7:
                 std::cout << "Teachers" << std::endl;
+                std::cout << "Coming soon" << std::endl;
                 break;
             case 0:
                 std::cout << "Exit" << std::endl;
@@ -113,4 +103,193 @@ void menu(){
                 break;
         }
     }
-}
+};
+
+int dialog(const char *msgs[], const int size){
+    for(int i = 0; i < size; i++){
+        std::cout << msgs[i] << std::endl;
+    }
+    std::cout << "Make your choice:" << std::endl;
+    int choice;
+    std::cin >> choice;
+    return choice;
+};
+
+template <class T, template <class> class Sequence> void createStack(Stack <T, Sequence> &stack){
+    std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
+    int len = 0;
+    while(len <= 0){
+        std::cin >> len;
+    }
+    for(int i = 0; i < len; i++){
+        std::cout << "Введите элемент" << std::endl;
+        T n;
+        std::cin >> n;
+        stack.push(n);
+    }
+};
+
+template <class T, template <class> class Sequence> void functionChoisenWhere(Stack <T, Sequence> *stack,  bool function(T, T)){
+    std::cout << "Введите элемент для сравнения" << std::endl;
+    T n;
+    std::cin >> n;
+    stack -> where(function, n);
+    stack -> printStack();
+};
+
+template <class T, template <class> class Sequence> void functionForWhere(Stack <T, Sequence> *stack){
+    bool choiceFunction = true;
+    int a = 0;
+    while(choiceFunction){
+        a = dialog(WHERE, WHERE_SIZE);
+        switch (a) {
+            case 0:
+                std::cout << "Exit" << std::endl;
+                choiceFunction = false;
+                break;
+            case 1:
+                {
+                    functionChoisenWhere(stack, isSmaller);
+                }
+                break;
+            case 2:
+                {
+                    functionChoisenWhere(stack, isBigger);
+                }
+                break;
+            case 3:
+                {
+                    functionChoisenWhere(stack, isEqual);
+                }
+                break;
+            case 4:
+                {
+                    functionChoisenWhere(stack, isUnEqual);
+                }
+                break;
+            default:
+                choiceFunction = false;
+                break;
+        };
+    };
+
+};
+
+template <class T, template <class> class Sequence> void functionChoisenMap(Stack <T, Sequence> *stack,  T function(T, T)){
+    std::cout << "Введите элемент для выполнения дейсвий со стеком" << std::endl;
+    T n;
+    std::cin >> n;
+    stack -> map(function, n);
+    stack -> printStack();
+};
+
+template <class T, template <class> class Sequence> void functionChoisenReduce(Stack <T, Sequence> *stack,  T function(T, T)){
+    std::cout << "Введите элемент для выполнения дейсвий со стеком" << std::endl;
+    T n;
+    std::cin >> n;
+    std::cout << stack -> reduce(function, n) << std::endl;
+};
+
+template <class T, template <class> class Sequence> void functionForMapReduce(Stack <T, Sequence> *stack, void functionChoisen(Stack <T, Sequence> *stack, T function (T, T))){
+    bool choiceFunction = true;
+    int a = 0;
+    while(choiceFunction){
+        a = dialog(MAP, MAP_SIZE);
+        switch (a) {
+            case 0:
+                std::cout << "Exit" << std::endl;
+                choiceFunction = false;
+                break;
+            case 1:
+                {
+                    functionChoisen(stack, addition);
+                }
+                break;
+            case 2:
+                {
+                    functionChoisen(stack, subtraction);
+                }
+                break;
+            case 3:
+                {
+                    functionChoisen(stack, multiply);
+                }
+                break;
+            case 4:
+                {
+                    functionChoisen(stack, division);
+                }
+                break;
+            default:
+                choiceFunction = false;
+                break;
+          };
+     };
+};
+
+template <class T, template <class> class Sequence> void functionForStack(Stack <T, Sequence> *stack){
+    bool choiceFunction = true;
+    int a = 0;
+    while(choiceFunction){
+        a = dialog(FUNCTION, FUNCTION_SIZE);
+        switch (a) {
+            case 1:
+                std::cout << "Map" << std::endl;
+                functionForMapReduce(stack, functionChoisenMap);
+                break;
+            case 2:
+                std::cout << "Where" << std::endl;
+                functionForWhere(stack);
+                break;
+            case 3:
+                std::cout << "Reduce" << std::endl;
+                functionForMapReduce(stack, functionChoisenReduce);;
+                break;
+            case 4:
+                std::cout << "Concatenation" << std::endl;
+                {
+                    Stack <T, Sequence> *newStack = new Stack <T, Sequence> ();
+                    createStack <T, Sequence> (*newStack);
+                    newStack -> printStack();
+                    Stack <T, Sequence> *concatStack = new Stack <T, Sequence> ();
+                    stack -> concatStack(newStack, *concatStack);
+                    concatStack -> printStack();
+                    delete newStack;
+                    delete concatStack;
+                }
+                break;
+            case 5:
+                std::cout << "SubSequence(index)" << std::endl;
+                {
+                    Stack <T, Sequence> *newStack = new Stack <T, Sequence> ();
+                    int start, end;
+                    std::cout << "Введите начальный индекс" << std::endl;
+                    std::cin >> start;
+                    std::cout << "Введите конечный индекс" << std::endl;
+                    std::cin >> end;
+                    stack -> subStack(start, end,*newStack);
+                    newStack -> printStack();
+                    delete newStack;
+                }
+                break;
+            case 6:
+                std::cout << "Find SubSequence" << std::endl;
+                {
+                    Stack <T, Sequence> *newStack = new Stack <T, Sequence> ();
+                    createStack <T, Sequence> (*newStack);
+                    newStack -> printStack();
+                    std::cout << "Начальная позиция - " << stack -> findSubStack(newStack) << std::endl;
+                    delete newStack;
+                }
+                break;
+            case 0:
+                std::cout << "Exit" << std::endl;
+                choiceFunction = false;
+                break;
+            default:
+                std::cout << "Error" << std::endl;
+                choiceFunction = false;
+                break;
+        };
+    };
+};
