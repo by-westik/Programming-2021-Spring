66f7167b197362323e7e332b45bda8285a5cdd6d
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-08 11:24:50 +0300

    Update FunctionForMap.h

--- lab_2/FunctionForMap.h
+++ lab_2/FunctionForMap.h
@@ -9,18 +9,43 @@ template <typename T> T multiply(T a, T b){
     return a * b;
 }
 
+template <> std::string multiply(std::string a, std::string b){
+    return a + b;
+}
+
 template <typename T> T division(T a, T b){
     return a / b;
 }
+/*
+template <> char division(char a, char b){
+    return a + b;
+}*/
+template <> std::string division(std::string a, std::string b){
+    return a + b;
+}
 
 template <typename T> T addition(T a, T b){
+    return a + b;
+}
+/*
+template <> char addition(char a, char b){
+    return a + b;
+}*/
+
+template <> std::string addition(std::string a, std::string b){
     return a + b;
 }
 
 template <typename T> T subtraction(T a, T b){
     return a - b;
 }
+/*
+template <> char subtraction(char a, char b){
+    return a + b;
+}*/
+
+template <> std::string subtraction(std::string a, std::string b){
+    return a + b;
+}
 
 #endif //LABA_2_FUNCTIONFORMAP_H
-
-
