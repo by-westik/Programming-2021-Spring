1bac0890b07cf8f48a75c8792dc2d48fbaf452b3
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-08 11:22:12 +0300

    Update classesForStack.cpp

--- lab_2/classesForStack.cpp
+++ lab_2/classesForStack.cpp
@@ -3,6 +3,51 @@
 
 #include "classesForStack.h"
 
+template <class typeOne, class typeTwo> std::ostream& operator<< (std::ostream &out, const std::pair<typeOne,typeTwo> &_pair){
+    out << "[ " << _pair.first << " " << _pair.second << " ]";
+    return out;
+};
+
+template <class typeOne, class typeTwo> std::pair<typeOne, typeTwo> operator+ (const std::pair<typeOne, typeTwo> &_pairL, const std::pair<typeOne, typeTwo> &_pairR){
+    return {_pairL.first + _pairR.first, _pairL.second + _pairR.second};
+};
+
+template <class typeOne, class typeTwo> std::pair<typeOne, typeTwo> operator- (const std::pair<typeOne, typeTwo> &_pairL, const std::pair<typeOne, typeTwo> &_pairR){
+    return {_pairL.first - _pairR.first, _pairL.second - _pairR.second};
+};
+
+template <class typeOne, class typeTwo> std::pair<typeOne, typeTwo> operator* (const std::pair<typeOne, typeTwo> &_pairL, const std::pair<typeOne, typeTwo> &_pairR){
+    return {_pairL.first * _pairR.first, _pairL.second * _pairR.second};
+};
+
+template <class typeOne, class typeTwo> std::pair<typeOne, typeTwo> operator/ (const std::pair<typeOne, typeTwo> &_pairL, const std::pair<typeOne, typeTwo> &_pairR){
+    return {_pairL.first / _pairR.first, _pairL.second / _pairR.second};
+};
+
+template <class typeOne, class typeTwo> std::pair<typeOne, typeTwo> operator+= (std::pair<typeOne, typeTwo> &_pairL, const std::pair<typeOne, typeTwo> &_pairR){
+    return {_pairL.first += _pairR.first, _pairL.second += _pairR.second};
+};
+
+template <class typeOne, class typeTwo> std::istream& operator>> (std::istream &in, std::pair<typeOne,typeTwo> &_pair){
+    std::cout << "Введите значение элемента" << std::endl;
+    typeOne _first;
+    in >> _first;
+    _pair.first = _first;
+    typeTwo _second;
+    in >> _second;
+    _pair.second = _second;
+    return in;
+};
+
+template <> std::istream& operator>> (std::istream &in, std::pair<int,int> &_pair){
+    std::cout << "Введите значение элемента" << std::endl;
+    int _first;
+    in >> _first;
+    _pair.first = _first;
+    _pair.second = _first;
+    return in;
+};
+
 Person::Person(int _age, std::string _firstName, std::string _middleName, std::string _lastName)
     :firstName(std::move(_firstName))
     ,middleName(std::move(_middleName))
@@ -10,7 +55,6 @@ Person::Person(int _age, std::string _firstName, std::string _middleName, std::s
     ,age(_age)
     {
     };
-
 std::string Person::getFirstName() const{
     return firstName;
 };
@@ -21,11 +65,6 @@ std::string Person::getMiddleName() const{
 
 std::string Person::getLastName() const{
     return lastName;
-};
-
-void Person::printPerson() const{
-    std::cout << "FIO - " << getLastName() << " " << getFirstName() << " " << getMiddleName() <<std::endl;
-    std::cout << "Age - " << getAge() << std::endl;
 };
 
 int Person::getAge() const{
@@ -48,12 +87,59 @@ void Person::setAge(int _age){
     this -> age = _age;
 };
 
+void Person::printPerson() const{
+    std::cout << "FIO - " << getLastName() << " " << getFirstName() << " " << getMiddleName() <<std::endl;
+    std::cout << "Age - " << getAge() << std::endl;
+};
+
+Person Person::operator+ (const Person &_person) const{
+    Person result;
+    result.setFirstName(firstName + _person.firstName);
+    result.setMiddleName(middleName + _person.middleName);
+    result.setLastName(lastName + _person.lastName);
+    result.setAge(age  + _person.age);
+    return result;
+};
+
+Person Person::operator- (const Person &_person) const{
+    Person result(age, firstName, middleName, lastName);
+    result.setAge(age  - _person.age);
+    return result;
+};
+
+Person Person::operator* (const Person &_person) const{
+    Person result(age, firstName, middleName, lastName);
+    result.setAge(age  * _person.age);
+    return result;
+};
+
+Person Person::operator/ (const Person &_person) const{
+    Person result(age, firstName, middleName, lastName);
+    result.setAge(age  / _person.age);
+    return result;
+};
+
+Person& Person::operator+= (const Person &_person){
+    firstName += _person.firstName;
+    middleName += _person.middleName;
+    lastName += _person.lastName;
+    age += _person.age;
+    return *this;
+};
+
 bool Person::operator== (Person &_person) const{
     return ((this -> getFirstName() == _person.getFirstName()) && (this -> getLastName() == _person.getLastName())
         && (this -> getMiddleName() == _person.getMiddleName()) && (this -> getAge() == _person.getAge()));
 };
-// Эти перегрузки оператора работают, но есть немного ниже вопрос
-/*
+
+bool Person::operator< (Person &_person) const{
+    return (this -> getAge() < _person.getAge());
+};
+
+bool Person::operator> (Person &_person) const{
+    return (this -> getAge() > _person.getAge());
+};
+
 std::istream& operator>> (std::istream &in, Person &_person){
     std::cout << "Введите имя"<< std::endl;
     std::string _firstName, _middleName, _lastName;
@@ -70,30 +156,13 @@ std::istream& operator>> (std::istream &in, Person &_person){
     in >> _age;
     _person.setAge(_age);
     return in;
-};*/
-
-void Person::createPerson(){
-    std::cout << "Введите имя"<< std::endl;
-    std::string _firstName, _middleName, _lastName;
-    std::cin >> _firstName;
-    this -> setFirstName(_firstName);
-    std::cout << "Введите фамилию"<< std::endl;
-    std::cin >> _lastName;
-    this -> setLastName(_lastName);
-    std::cout << "Введите отчество"<< std::endl;
-    std::cin >> _middleName;
-    this -> setMiddleName(_middleName);
-    int _age;
-    std::cout << "Введите возраст"<< std::endl;
-    std::cin >> _age;
-    this -> setAge(_age);
 };
 
-std::istream& operator>> (std::istream &in, Person &_person){
-    _person.createPerson();
-    return in;
+std::ostream& operator<< (std::ostream &out, const Person &_person){
+    out << "FIO - " << _person.getLastName() << " " << _person.getFirstName() << " " << _person.getMiddleName() <<std::endl;
+    out << "Age - " << _person.getAge() << std::endl;
+    return out;
 };
-
 
 Student::Student(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _group)
     :Person(_age, _firstName, _middleName, _lastName)
@@ -114,25 +183,62 @@ void Student::printStudent() const{
     std::cout << "Group - " << getGroup() << std::endl;
 };
 
-bool Student::operator==(Student &_student) const{
-    return ((this -> getFirstName() == _student.getFirstName()) && (this -> getLastName() == _student.getLastName())
-        && (this -> getMiddleName() == _student.getMiddleName()) && (this -> getAge() == _student.getAge()) && (this -> getGroup() == _student.getGroup()));
-//    return ((*this == _student) && (this -> getGroup() == _student.getGroup()));
-// Я никак не смогу использовать == перегруженный в Person для == в Student/Teacher?
+Student Student::operator+ (const Student &_student) const{
+    Student result(this -> getAge() + _student.getAge(), this -> getFirstName() + _student.getFirstName(),
+                   this -> getMiddleName() + _student.getMiddleName(), this -> getLastName() + _student.getLastName(), this -> getGroup() + _student.getGroup());
+    return result;
 };
 
-bool Student::operator>(Student &_student) const{
+Student Student::operator- (const Student &_student) const{
+    Student result(this -> getAge() - _student.getAge(), this -> getFirstName(),this -> getMiddleName(), this -> getLastName(), this -> getGroup());
+    return result;
+};
+
+
+Student Student::operator* (const Student &_student) const{
+    Student result(this -> getAge() * _student.getAge(), this -> getFirstName(),this -> getMiddleName(), this -> getLastName(), this -> getGroup());
+    return result;
+};
+
+Student Student::operator/ (const Student &_student) const{
+    Student result(this -> getAge() / _student.getAge(), this -> getFirstName(),this -> getMiddleName(), this -> getLastName(), this -> getGroup());
+    return result;
+};
+
+Student& Student::operator+= (const Student &_student){
+    this -> setFirstName(_student.getFirstName() + this -> getFirstName());
+    this -> setMiddleName(_student.getMiddleName() + this -> getMiddleName());
+    this -> setLastName(_student.getLastName() + this -> getLastName());
+    this -> setGroup(_student.getGroup() + this -> getGroup());
+    this -> setAge(_student.getAge() + this -> getAge());
+    return *this;
+};
+
+bool Student::operator==(Student &_student){
+    return ((dynamic_cast <Person&> (*this) == dynamic_cast <Person&> (_student)) && (this -> getGroup() == _student.getGroup()));
+};
+
+bool Student::operator< (Student &_student) const{
+    return (this -> getAge() < _student.getAge());
+};
+
+bool Student::operator> (Student &_student) const{
     return (this -> getAge() > _student.getAge());
 };
 
 std::istream& operator>> (std::istream &in, Student &_student){
- //   in >> _student; //И тут тоже не получиться вызвать >> для Person?
-    _student.createPerson();
+    in >> dynamic_cast <Person&> (_student);
     std::string _group;
     std::cout << "Введите группу" << std::endl;
     in >> _group;
     _student.setGroup(_group);
     return in;
+}
+
+std::ostream& operator<< (std::ostream &out, Student &_student){
+    out << dynamic_cast <Person&> (_student);
+    out << "Group - " << _student.getGroup() << std::endl;
+    return out;
 };
 
 Teacher::Teacher(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _subject)
@@ -145,26 +251,59 @@ std::string Teacher::getSubject() const{
     return subject;
 };
 
+void Teacher::setSubject(std::string _subject){
+    this -> subject = _subject;
+};
+
 void Teacher::printTeacher() const{
     printPerson();
     std::cout << "Subject - " << getSubject() << std::endl;
 };
 
-void Teacher::setSubject(std::string _subject){
-    this -> subject = _subject;
+Teacher Teacher::operator+ (const Teacher &_teacher) const{
+    Teacher result(this -> getAge() + _teacher.getAge(), this -> getFirstName() + _teacher.getFirstName(),
+                   this -> getMiddleName() + _teacher.getMiddleName(), this -> getLastName() + _teacher.getLastName(), this -> getSubject() + _teacher.getSubject());
+    return result;
 };
 
-bool Teacher::operator==(Teacher &_teacher) const{
-    return ((this -> getFirstName() == _teacher.getFirstName()) && (this -> getLastName() == _teacher.getLastName())
-        && (this -> getMiddleName() == _teacher.getMiddleName()) && (this -> getAge() == _teacher.getAge()) && (this -> getSubject() == _teacher.getSubject()));
+Teacher Teacher::operator- (const Teacher &_teacher) const{
+    Teacher result(this -> getAge() - _teacher.getAge(), this -> getFirstName(), this -> getMiddleName(), this -> getLastName(), this -> getSubject());
+    return result;
 };
 
-bool Teacher::operator>(Teacher &_teacher) const{
+Teacher Teacher::operator* (const Teacher &_teacher) const{
+    Teacher result(this -> getAge() * _teacher.getAge(), this -> getFirstName(), this -> getMiddleName(), this -> getLastName(), this -> getSubject());
+    return result;
+};
+
+Teacher Teacher::operator/ (const Teacher &_teacher) const{
+    Teacher result(this -> getAge() / _teacher.getAge(), this -> getFirstName(), this -> getMiddleName(), this -> getLastName(), this -> getSubject());
+    return result;
+};
+
+Teacher& Teacher::operator+= (const Teacher &_teacher){
+    this -> setFirstName(_teacher.getFirstName() + this -> getFirstName());
+    this -> setMiddleName(_teacher.getMiddleName() + this -> getMiddleName());
+    this -> setLastName(_teacher.getLastName() + this -> getLastName());
+    this -> setSubject(_teacher.getSubject() + this -> getSubject());
+    this -> setAge(_teacher.getAge() + this -> getAge());
+    return *this;
+};
+
+bool Teacher::operator==(Teacher &_teacher){
+    return ((dynamic_cast <Person&> (*this) == dynamic_cast <Person&> (_teacher)) && (this -> getSubject() == _teacher.getSubject()));
+}
+
+bool Teacher::operator< (Teacher &_teacher) const{
+    return (this -> getAge() < _teacher.getAge());
+};
+
+bool Teacher::operator> (Teacher &_teacher) const{
     return (this -> getAge() > _teacher.getAge());
 };
 
 std::istream& operator>> (std::istream &in, Teacher &_teacher){
-    _teacher.createPerson();
+    in >> dynamic_cast <Person&> (_teacher);
     std::string _subject;
     std::cout << "Введите преподаваемый предмет" << std::endl;
     in >> _subject;
@@ -172,11 +311,43 @@ std::istream& operator>> (std::istream &in, Teacher &_teacher){
     return in;
 };
 
+std::ostream& operator<< (std::ostream &out, Teacher &_teacher){
+    out << dynamic_cast <Person&> (_teacher);
+    out << "Subject - " << _teacher.getSubject() << std::endl;
+    return out;
+};
+
+Complex::Complex()
+    :real(0)
+    ,imaginary(0)
+    {
+    };
+
 Complex::Complex(double _real, double _imaginary)
     :real(_real)
     ,imaginary(_imaginary)
     {
     };
+
+double Complex::getReal() const{
+    return real;
+};
+
+double Complex::getImaginary() const{
+    return imaginary;
+};
+
+void Complex::setReal(double _real){
+    this -> real = _real;
+};
+
+void Complex::setImaginary(double _imaginary){
+    this -> imaginary = _imaginary;
+};
+
+void Complex::printComplex() const{
+    std::cout << getReal() << "x + " << getImaginary() << "y" << std::endl;
+};
 
 Complex Complex::operator+ (const Complex &_complex) const{
     Complex result;
@@ -237,30 +408,8 @@ std::istream& operator>> (std::istream &in, Complex &complex){
 };
 
 std::ostream& operator<< (std::ostream &out, const Complex &complex){
-    out << complex.getReal() << "x + " << complex.getImaginary() << "y" << std::endl;
+    out << " " <<complex.getReal() << "x + " << complex.getImaginary() << "y ";
     return out;
 };
 
-double Complex::getReal() const{
-    return real;
-};
-
-double Complex::getImaginary() const{
-    return imaginary;
-};
-
-void Complex::setReal(double _real){
-    this -> real = _real;
-};
-
-void Complex::setImaginary(double _imaginary){
-    this -> imaginary = _imaginary;
-};
-
-void Complex::printComplex() const{
-    std::cout << getReal() << "x + " << getImaginary() << "y" << std::endl;
-};
-
 #endif //LABA_2_CLASSESFORSTACK_H
-
-
