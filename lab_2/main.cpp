03a51c1ebc0a78eb14f425a756afcfaa3873d7b1
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-08 11:33:37 +0300

    Update main.cpp

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
+#include "menu.cpp"
 #include <string>
-#include <time.h>
-//#include "Stack.h"
-//#include "FunctionForWhere.h"
-//#include "functional"
 #include <utility>
-#include "DynamicArray.h"
-
 using namespace std;
 
+
 int main () {
-    int array[5] = {1, 2, 3, 4, 5};
-    DynamicArray <int> n(array, 5);
-    n.printArray();
-    try{
-        cout << n[0] << endl;
-    }
-    catch (Error &error){
-        cout << "Error: " << error.getError() << endl;
-    }
+    mainMenu();
+    return 0;
 }
