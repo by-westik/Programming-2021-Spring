d4ac90ace1b6a4a24214ef336f048cd1157c570f
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-08 11:23:38 +0300

    Update classesForStack.h

--- lab_2/classesForStack.h
+++ lab_2/classesForStack.h
@@ -11,6 +11,8 @@ private:
     std::string lastName;
     int age;
 public:
+    Person(){};
+    Person(int _age, std::string _firstName, std::string _middleName, std::string _lastName);
     std::string getFirstName() const;
     std::string getMiddleName() const;
     std::string getLastName() const;
@@ -19,23 +21,33 @@ public:
     void setMiddleName(std::string _middleName);
     void setLastName(std::string _lastName);
     void setAge(int _age);
-    Person(){};
-    Person(int _age, std::string _firstName, std::string _middleName, std::string _lastName);
     void printPerson() const;
-    void createPerson();
+    Person operator+ (const Person &_person) const;
+    Person operator- (const Person &_person) const;
+    Person operator* (const Person &_person) const;
+    Person operator/ (const Person &_person) const;
+    Person& operator+= (const Person &_person);
     bool operator== (Person &person) const;
+    bool operator< (Person &person) const;
+    bool operator> (Person &person) const;
 };
 
 class Student: public Person{
 private:
     std::string group;
 public:
-    std::string getGroup() const;
-    void setGroup(std::string _group);
     Student(){};
     Student(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _group);
+    std::string getGroup() const;
+    void setGroup(std::string _group);
     void printStudent() const;
-    bool operator== (Student &_student) const;
+    Student operator+ (const Student &_student) const;
+    Student operator- (const Student &_student) const;
+    Student operator* (const Student &_student) const;
+    Student operator/ (const Student &_student) const;
+    Student& operator+= (const Student &_student);
+    bool operator== (Student &_student);
+    bool operator< (Student &_student) const;
     bool operator> (Student &_student) const;
 };
 
@@ -43,23 +55,33 @@ class Teacher: public Person{
 private:
     std::string subject;
 public:
-    std::string getSubject() const;
-    void setSubject(std::string _subject);
     Teacher(){};
     Teacher(int _age, std::string _firstName, std::string _middleName, std::string _lastName, string _subject);
+    std::string getSubject() const;
+    void setSubject(std::string _subject);
     void printTeacher() const;
-    bool operator== (Teacher &_teacher) const;
+    Teacher operator+ (const Teacher &_teacher) const;
+    Teacher operator- (const Teacher &_teacher) const;
+    Teacher operator* (const Teacher &_teacher) const;
+    Teacher operator/ (const Teacher &_teacher) const;
+    Teacher& operator+= (const Teacher &_teacher);
+    bool operator== (Teacher &_teacher);
+    bool operator< (Teacher &_teacher) const;
     bool operator> (Teacher &_teacher) const;
 };
-
 
 class Complex{
 private:
     double real;
     double imaginary;
 public:
-    Complex(){};
+    Complex();
     Complex(double _real, double _imaginary);
+    double getReal() const;
+    double getImaginary() const;
+    void setReal(double _real);
+    void setImaginary(double _imaginary);
+    void printComplex() const;
     Complex operator+ (const Complex &_complex) const;
     Complex operator- (const Complex &_complex) const;
     Complex operator* (const Complex &_complex) const;
@@ -68,11 +90,6 @@ public:
     bool operator== (const Complex &_complex) const;
     bool operator< (const Complex &_complex) const;
     bool operator> (const Complex &_complex) const;
-    double getReal() const;
-    double getImaginary() const;
-    void setReal(double _real);
-    void setImaginary(double _imaginary);
-    void printComplex() const;
 };
 
 #endif //LABA_2_CLASSESFORSTACK_H
