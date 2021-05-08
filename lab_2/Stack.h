6a036e043d0757355ec36854a85254ae18b757ae
by-westik <78916184+by-westik@users.noreply.github.com>
2021-05-08 11:17:24 +0300

    Update Stack.h

--- lab_2/Stack.h
+++ lab_2/Stack.h
@@ -1,19 +1,18 @@
-
 //
 // Created by User on 09.04.2021.
 //
-#include "Sequence.h"
+
 #ifndef LABA_2_STACK_H
 #define LABA_2_STACK_H
+#include "Sequence.h"
 #include "FunctionForMap.h"
 #include "FunctionForWhere.h"
-
 
 template <class T, template <class> class Sequence> class Stack {
 public:
     Sequence <T> *list;
     Stack()
-    :list(new Sequence<T>())
+    :list(new Sequence <T>())
     {
     };
     Stack(const T* _list, int _size)
@@ -24,11 +23,11 @@ public:
     :list(new Sequence<T>(*(stack.list)))
     {
     };
-    ~Stack(){
+    virtual ~Stack(){
         delete list;
-    }
+    };
     bool isEmpty(){
-        if(!(this -> list -> data -> size)){
+        if(!(this -> list -> getLength())){
             return true;
         };
         return false;
@@ -48,101 +47,92 @@ public:
     void pop(){
         this -> list -> deleteFirst();
     };
-    void subStack(int startIndex, int endIndex, Stack <T, Sequence> &subStack){
-        this -> list -> getSubsequence(startIndex, endIndex, *subStack.list);
-    }
-    void concatStack(Stack <T, Sequence> *stack, Stack <T, Sequence> &concatStack){
-        this -> list -> concat(stack -> list, *concatStack.list);
-    };
-    void map(T function(T , T), T n){
+    void map(T function(T, T), T n){
+        if(isEmpty())
+            throw Error(ERROR[2]);
         Sequence <T> *newList = new Sequence <T> ();
         for(int i = 0; i < this -> getSize(); i++){
             newList -> prepend(function(this -> list -> get(i), n));
         }
         delete list;
         this -> list = newList;
-    }
-    void where(bool function(T), Stack <T, Sequence> &newStack){
+    };
+    void where(bool function(T, T), T n){
+        if(isEmpty())
+            throw Error(ERROR[2]);
+        Sequence <T> *newList = new Sequence <T> ();
         for(int i = 0; i < this -> getSize(); i++){
-            if(function(this -> list -> get(i))){
-                newStack.push(this -> list -> get(i));
-            }
-        }
-    }
-    //Работающий reduce)))
+            if(function(this -> list -> get(i), n)){
+                newList -> prepend(this -> list -> get(i));
+            };
+        };
+        delete list;
+        this -> list = newList;
+    };
     T reduce(T function(T, T), T n){
+        if(isEmpty())
+            throw Error(ERROR[2]);
         T result = n;
         for(int i = 0; i < this -> getSize(); i++){
             result += function((this -> list -> get(i)), result);
-        }
+        };
         return result;
-    }
-    //Поиск подстека в стеке
+    };
+    void subStack(int startIndex, int endIndex, Stack <T, Sequence> &subStack){
+        if((startIndex < 0) || (endIndex < 0) || (startIndex >= getSize()) || (endIndex >= getSize()) || (startIndex > endIndex))
+            throw Error(ERROR[0]);
+        if(this -> isEmpty())
+            throw Error(ERROR[2]);
+        this -> list -> getSubsequence(startIndex, endIndex, *subStack.list);
+    };
+    void concatStack(Stack <T, Sequence> *stack, Stack <T, Sequence> &concatStack){
+        if(stack -> isEmpty())
+            throw Error(ERROR[2]);
+        this -> list -> concat(stack -> list, *concatStack.list);
+    };
     int findSubStack(Stack <T, Sequence> *subStack){
+        if(this -> isEmpty() || (subStack -> isEmpty()))
+            throw Error(ERROR[2]);
+        if(this -> getSize() < subStack -> getSize())
+            throw Error(ERROR[4]);
         for(int i = 0; i < this -> getSize(); i++){
              int j = i, n = 0;
              while((j < this -> getSize()) && (n < subStack -> getSize()) && (this -> list -> get(j) ==  subStack -> list -> get(n))){
                  n++;
                  j++;
              };
-             std::cout << n << " " << subStack -> getSize() << std::endl;
              if(n == subStack -> getSize()){
                  return i;
-             }
+             };
         };
-    }
-    /*    template <T> void createStack(){
-        std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
-        int len = 0;
-        while(len <= 0){
-            std::cin >> len;
-            }
-        for(int i = 0; i < len; i++){
-            std::cout << "Введите элемент" << std::endl;
-            T n = 0;
-            std::cin >> n;
-            this -> push(n);
+        return -1;
+    };
+};
+
+template <> void Stack<std::pair<int,int>,LinkedListSequence>::push(std::pair<int, int> item){
+    if(isEmpty()){
+        this -> list -> append(item);
+    }else{
+        std::pair<int, int> head = this -> top();
+        if(item.second >= head.second){
+             this -> list -> append(item);
+        }else{
+             this -> list -> append(std::pair<int,int> (item.first, head.second));
         }
-    }*/
+    }
 };
 
-/*
-template <> void Stack<Complex>::createStack(){
-    std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
-    int len = 0;
-    while(len <= 0){
-        std::cin >> len;
-    }
-    for(int i = 0; i < len; i++){
-        std::cout << "Введите x" << std::endl;
-        double r, im;
-        std::cin >> r;
-        std::cout << "Введите y" << std::endl;
-        std::cin >> im;
-        this -> push(Complex(r,im));
+template <> void Stack<std::pair<int,int>,ArraySequence>::push(std::pair<int, int> item){
+    if(isEmpty()){
+        this -> list -> append(item);
+    }else{
+        std::pair<int, int> head = this -> top();
+        if(item.second >= head.second){
+             this -> list -> append(item);
+        }else{
+             this -> list -> append(std::pair<int,int> (item.first, head.second));
+        }
     }
 };
-*/
-//Я пока не знаю, как это исправить. Ноо, если я вот так запишу функцию
-//template <> void Stack<Complex, Sequence>::createStack() , то все ломается, то есть вызываетя верхняя функция для простых типов
-//С чем это может быть связано? Или как это исправить? (｡╯︵╰｡)
-/*
-template <> void Stack<Complex, ArraySequence>::createStack(){
-    std::cout << "Сколько элементов Вы хотите добавить в стек?" << std::endl;
-    int len = 0;
-    while(len <= 0){
-        std::cin >> len;
-    }
-    for(int i = 0; i < len; i++){
-        std::cout << "Введите x" << std::endl;
-        double r, im;
-        std::cin >> r;
-        std::cout << "Введите y" << std::endl;
-        std::cin >> im;
-        this -> push(Complex(r,im));
-    }
-};*/
-};
-
 
 #endif //LABA_2_STACK_H
